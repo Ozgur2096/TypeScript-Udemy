ef26c0c0968c9d802c646c978d8362cc50f1db4f
Ozgur Ozhan <ozgur2096@gmail.com>
2023-10-03 16:49:11 +0200

    video27

--- course-starting-project/course-starting-project/app.ts
+++ course-starting-project/course-starting-project/app.ts
@@ -21,3 +21,12 @@ combineValues = add;
 // combineValues = printResult; // Compilation Error
 
 console.log(combineValues(8, 8));
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, result => {
+  console.log(result);
+});

6586123213dcd49d8909d38b37ab439d901be24b
Ozgur Ozhan <ozgur2096@gmail.com>
2023-10-04 15:22:35 +0200

    ts compiler and its configuration

--- course-starting-project/course-starting-project/app.js
+++ course-starting-project/course-starting-project/app.js
@@ -1,22 +1,15 @@
 // The "UNKNOWN" type
-
-let userInput: unknown;
-let userName: string;
-
+var userInput;
+var userName;
 userInput = 5;
 userInput = 'a';
-
 // userInput: string; // Error
 // if the type of userInput would be "any", it would be defined as any type
-
 if (typeof userInput === 'string') {
-  userName = userInput;
+    userName = userInput;
 }
-
 // The "NEVER" type
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-
 generateError('An error occurred', 500);
---
+++ section-3/app.js
@@ -0,0 +1,3 @@
+"use strict";
+const userName = 'Ozgur';
+console.log(userName);

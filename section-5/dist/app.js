cdc1dce423212dd74c4044b64cfd6f8bd4e92cb9
Ozgur Ozhan <ozgur2096@gmail.com>
2023-10-10 14:49:04 +0200

    interfaces

--- section-5/dist/app.js
+++ section-5/dist/app.js
@@ -5,6 +5,9 @@ class Department {
         this.employees = [];
         this.name = n;
     }
+    static createEmployee(name) {
+        return { name };
+    }
     describe() {
         console.log('Department: ' + this.name);
         console.log('Department Id: ' + this.departmentId);
@@ -17,8 +20,60 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department('Accounting', 'id1');
+Department.fiscalYear = 2023;
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super('IT', id);
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found');
+    }
+    set mostRecentReport(report) {
+        if (!report) {
+            throw new Error('Please pass in a valid report');
+        }
+        this.addReport(report);
+    }
+    constructor(id, reports) {
+        super('Accounting', id);
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    addEmployee(employee) {
+        if (employee === 'Ozgur') {
+            return;
+        }
+        this.employees.push(employee);
+    }
+    addReport(report) {
+        this.reports.push(report);
+        this.lastReport = report;
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const IT = new ITDepartment('id2', ['John']);
+IT.addEmployee('Max');
+console.log(IT);
+const accounting = new AccountingDepartment('id1', ['New Reports']);
+accounting.addReport('Second Report');
+console.log(accounting);
 accounting.addEmployee('John');
 accounting.addEmployee('Jane');
+accounting.addEmployee('Ozgu');
+accounting.addEmployee('Ozgur');
 accounting.describe();
 accounting.printEmployeeInformation();
+console.log(accounting.mostRecentReport);
+accounting.mostRecentReport = 'The newest report';
+console.log(accounting.mostRecentReport);
+const newEmployee = Department.createEmployee('Elizabeth');
+console.log(newEmployee);
+console.log(Department.fiscalYear);

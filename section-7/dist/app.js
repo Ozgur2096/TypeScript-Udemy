ec7711155ead2d6c220493e7595a211da3443c9c
Ozgur Ozhan <ozgur2096@gmail.com>
2023-10-21 15:46:57 +0200

    next: keyof

--- section-7/dist/app.js
+++ section-7/dist/app.js
@@ -8,3 +8,16 @@ const promise = new Promise(resolve => {
 promise.then(result => {
     console.log(result);
 });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObject = merge({ name: 'Joe' }, { age: 30 });
+console.log(mergedObject.name);
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value';
+    if (element.length > 0)
+        descriptionText = 'Got ' + element.length + ' element(s)';
+    return [element, descriptionText];
+}
+console.log(countAndDescribe('Joe is going to the cinema'));
+console.log(countAndDescribe([1, 2, 3, 4, 'af', 'op']));
--- section-7/src/app.ts
+++ section-7/src/app.ts
@@ -1,6 +1,6 @@
 const names: Array<any> = [];
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
+const promise: Promise<string> = new Promise(resolve => {
   setTimeout(() => {
     resolve('success');
   }, 2000);
@@ -9,3 +9,26 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then(result => {
   console.log(result);
 });
+
+// Creating a Generic Function
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObject = merge({ name: 'Joe' }, { age: 30 });
+console.log(mergedObject.name);
+
+// another function example
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = 'Got no value';
+  if (element.length > 0)
+    descriptionText = 'Got ' + element.length + ' element(s)';
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Joe is going to the cinema'));
+console.log(countAndDescribe([1, 2, 3, 4, 'af', 'op']));
